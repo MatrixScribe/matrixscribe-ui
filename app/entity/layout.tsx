8dd420a178d4edb63bf8f85b54c73dc7b43c46a3
Suhail Surtee <surteesuhail@outlook.com>
2026-04-09 16:37:41 +0200

    XUX

--- app/entity/layout.tsx
+++
@@ -1,8 +0,0 @@
-// app/entity/layout.tsx
-
-import React from "react";
-
-export default function EntityLayout(props: any) {
-  // We don't need params here; just render children.
-  return <>{props.children}</>;
-}
