19d9caad99b8e8ffceaf642194d1e95589698f85
Suhail Surtee <surteesuhail@outlook.com>
2026-04-09 10:21:40 +0200

    XGX

--- app/entity/[slug]/page.tsx
+++ app/entity/[slug]/page.tsx
@@ -77,6 +77,7 @@ export default async function EntityPage({
 }: {
   params: { slug: string };
 }) {
+  const { slug } = params;
   // ------------------------------
   // 1. AUTH CHECK
   // ------------------------------
