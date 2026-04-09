1fc33444b2e11229ddca75956a42b8a5e20037bc
Suhail Surtee <surteesuhail@outlook.com>
2026-04-09 10:35:33 +0200

    XTX

--- app/entity/layout.tsx
+++ app/entity/layout.tsx
@@ -1,3 +1,9 @@
-export default function EntityLayout({ children }: { children: React.ReactNode }) {
+export default function EntityLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { slug?: string };
+}) {
   return <>{children}</>;
 }
