ff8050742762063567546b77eda4e89470d15b44
Suhail Surtee <surteesuhail@outlook.com>
2026-04-10 10:47:38 +0200

    XEX

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,15 +1,11 @@
 // ❌ NO "use client" HERE — this must be a Server Component
 
-import Sidebar from "@/app/components/Sidebar";
-import TopBar from "@/app/components/TopBar";
 import ClientShell from "./ClientShell";
 
 export default function PlatformLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: any;
 }) {
   return (
     <ClientShell>
