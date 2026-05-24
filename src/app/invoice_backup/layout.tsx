8a6ba8e79af78099f5072bb24c911a7196bbb0e2
Suhail Surtee <surteesuhail@outlook.com>
2026-05-24 08:21:26 +0200

    M1

--- src/app/invoice_backup/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function InvoiceLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen">
-      {children}
-    </div>
-  );
-}
--- src/app/rate_backup/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function RateLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen">
-      {children}
-    </div>
-  );
-}
