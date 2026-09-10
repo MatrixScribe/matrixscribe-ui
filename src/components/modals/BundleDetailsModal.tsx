3812de915ed12b816639a88ae838629ad0cbce39
Suhail Surtee <surteesuhail@outlook.com>
2026-09-10 11:41:00 +0200

    S38.15

--- src/components/modals/BundleDetailsModal.tsx
+++ src/components/modals/BundleDetailsModal.tsx
@@ -351,7 +351,7 @@ export default function BundleDetailsModal({
                     px-1 py-2
                   "
                 >
-                  {roamingCountries.map((c, idx) => (
+                  {roamingCountries.map((c: any, idx: number) => (
                     <div key={idx} className="flex-shrink-0">
                       {c.flag && (
                         <img
